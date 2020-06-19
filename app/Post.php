0059ca7b536b195190da8f8bd8d1ad45b0e87f39
xhevdet <xhevdet.kryeziu@kujtesa.com>
2020-06-19 10:28:13 +0200

    fetching data with eloquent

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // Table Name - (we dont need it but ill keep it for refrence)
+    protected $table = 'posts';
+    // Primary Key
+    public $primaryKey = 'id';
+    // Timestamps
+    public $timestamps = true;
+
+
+
 }
--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-5">
     <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -15,6 +15,9 @@
         <li class="nav-item">
           <a class="nav-link" href="/services">Services</a>
         </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/posts">Blog</a>
+          </li>
       </ul>
     </div>
   </nav>
